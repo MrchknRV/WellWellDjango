cc825af08f0c58006d3521c46c2f52bdc9274d08
MrchknRV <rodionmarochkin32@gmail.com>
2025-09-02 16:03:16 +0300

    added POST

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
@@ -6,4 +7,10 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get('phone')
+        message = request.POST.get("message")
+        print(f'{name} - {phone}: {message}')
+        return HttpResponse(f'Thank you {name}! Your message success.')
     return render(request, 'catalog/contacts.html')
