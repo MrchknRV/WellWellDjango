79fc18a2c58b1ac261efb8209725f6ddaf952679
MrchknRV <rodionmarochkin32@gmail.com>
2025-09-04 17:05:25 +0300

    added admin-user

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category_name']
+    search_fields = ['category_description', 'category_name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_name', 'product_price', 'category']
+    list_filter = ['category', 'created_at']
+    search_fields = ['product_name', 'product_description']
+    raw_id_fields = ['category']
--- catalog/models.py
+++ catalog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100, verbose_name='категория')
-    category_description = models.TextField()
+    category_name = models.CharField(max_length=100, verbose_name='Категория')
+    category_description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return self.category_name
@@ -15,13 +15,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=150, verbose_name='продукт')
-    product_description = models.TextField()
-    product_image = models.ImageField(upload_to='photos/', verbose_name='фотография')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    product_price = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    product_name = models.CharField(max_length=150, verbose_name='Продукт')
+    product_description = models.TextField(verbose_name='Описание')
+    product_image = models.ImageField(upload_to='photos/', verbose_name='Фотография', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products',
+                                 verbose_name='Категория продуктов')
+    product_price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавление')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
         return self.product_name
