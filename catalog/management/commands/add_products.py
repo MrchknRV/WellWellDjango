c2fa030faf6d68844282abec0ff3e684d614a6d4
MrchknRV <rodionmarochkin32@gmail.com>
2025-09-06 12:08:00 +0300

    added custom command for load product

---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Product
+
+class Command(BaseCommand):
+    help = 'Added test product to the BD'
+
+    def handle(self, *args, **options):
+        Product.object.all().delete()
+        call_command('loaddata', 'test_product_fixture.json')
