e8070d4a523c3ada2054f36a262714c3459d226d
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-09-16 10:46:22 +0300

    переведены имеющиеся в проекте контроллеры с FCB на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
 from catalog.models import Product
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get('phone')
-        message = request.POST.get("message")
-        print(f'{name} - {phone}: {message}')
-        return HttpResponse(f'Thank you {name}! Your message success.')
-    return render(request, 'catalog/contacts.html')
+from django.views.generic import ListView, DetailView, TemplateView
 
 
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'catalog/home.html', {'products': products})
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
 
-def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
-    return render(request, 'catalog/product.html', {'product': product})
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
